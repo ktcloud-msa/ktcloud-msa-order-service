3f9b8cda86f41332ea2c48fc6b4495e72245f554
K4NEI <kimyoungho0415@gmail.com>
2026-05-13 11:22:06 +0900

    Split modules [common order-service order] to ktcloud-msa-order-service

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -18,6 +18,9 @@ dependencies {
     runtimeOnly("com.h2database:h2")
 
     implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
+    kapt("jakarta.persistence:jakarta.persistence-api")
+    kapt("jakarta.annotation:jakarta.annotation-api")
 }
 
 sourceSets {
